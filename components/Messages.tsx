dcf65559d8e454595d5f649f0f4c49265fcae421
ayushmaninbox <ayushmanmohapatra895@gmail.com>
2024-12-10 13:39:30 +0530

    expression fom ai removed

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -56,7 +56,10 @@ const Messages = forwardRef<
                     {msg.message.role}
                   </div>
                   <div className={"pb-3 px-3"}>{msg.message.content}</div>
-                  <Expressions values={{ ...msg.models.prosody?.scores }} />
+                  {/* Only show expressions for user messages */}
+                  {msg.type === "user_message" && (
+                    <Expressions values={{ ...msg.models.prosody?.scores }} />
+                  )}
                 </motion.div>
               );
             }
